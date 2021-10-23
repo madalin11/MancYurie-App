410e015cd10cb60f0d38986fb3041195f43e99b9
madalin11 <madalin.c.puiu@gmail.com>
2021-10-23 17:59:20 +0300

    Add react navigation depedencies

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Text>Dada</Text>
       <StatusBar style="auto" />
     </View>
   );
