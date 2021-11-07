b7c8ead890685fcf8ffe64880e604a72fce1d94e
madalin11 <madalin.c.puiu@gmail.com>
2021-11-07 15:52:15 +0200

    Add input fields to register page without styling

--- Screens/Register.js
+++ Screens/Register.js
@@ -1,16 +1,70 @@
 import React from 'react'
-import { StyleSheet, Text, View,TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native'
 
-const Register = ({navigation}) => {
+const Register = ({ navigation }) => {
     return (
         <View>
-            <TouchableOpacity
-                onPress={()=>navigation.replace("Home screen")}
-            >
-                <Text>
-                    Register 
-                </Text>
-            </TouchableOpacity>
+            <View style={styles.inputContainer}>
+
+                <TextInput
+                    placeholder="Firs Name"
+                //value={}
+                //onChangeText={}
+                style={styles.input}
+                />
+                <TextInput
+                    placeholder="Last Name"
+                //value={}
+                //onChangeText={}
+                style={styles.input}
+                // secureTextEntry
+                />
+                <TextInput
+                    placeholder="Email"
+                    type="email"
+                //value={}
+                //onChangeText={}
+                style={styles.input}
+                />
+                <TextInput
+                    placeholder="Phone Number"
+                //value={}
+                //onChangeText={}
+                style={styles.input}
+                // secureTextEntry
+                />
+                <TextInput
+                    placeholder="Password"
+                    //value={}
+                    //onChangeText={}
+                    style={styles.input}
+                    secureTextEntry
+                />
+                <TextInput
+                    placeholder="Repete Password"
+                    //value={}
+                    //onChangeText={}
+                    style={styles.input}
+                    secureTextEntry
+                />
+
+            </View>
+            <View>
+                <TouchableOpacity
+                    onPress={() => navigation.replace("Home screen")}
+                >
+                    <Text>
+                        Register
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => navigation.replace("Login screen")}
+                >
+                    <Text>
+                        Back
+                    </Text>
+                </TouchableOpacity>
+            </View>
             <Text></Text>
         </View>
     )
@@ -18,4 +72,12 @@ const Register = ({navigation}) => {
 
 export default Register
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    input: {
+        backgroundColor: 'white',
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 10,
+        marginTop: 5,
+    }
+})
